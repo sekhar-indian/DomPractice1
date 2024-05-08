a1b7ff3c7f0ba441e3a7e55c776ed3b22539416c
sekhar <munisekharudavalapati1@gmail.com>
2024-05-08 14:56:33 +0530

    fruits list

--- dom3/fresh_fruit_shp.js
+++ dom3/fresh_fruit_shp.js
@@ -1,5 +1,26 @@
-const header =document.getElementById('headerE');
-header.style.backgroundColor="green";
-header.style.color="yellow";
-header.style.
-mainheading.style.backgroundColor="grren";
+const header=document.querySelector('#main-heading');
+header.style.textAlign='right';
+
+const listheading=document.querySelector('#basket-heading');
+listheading.style.color='brown';
+listheading.style.padding='10px';
+
+const lists=document.querySelector('.fruits');
+lists.style.backgroundColor='grey';
+lists.style.listStyleType='none';
+lists.style.borderRadius='10px';
+lists.style.padding='10px';
+
+const list=document.querySelectorAll('.fruit');
+for(let i=0;i<list.length-1;i=i+2){
+list[i].style.backgroundColor='white';
+list[i+1].style.backgroundColor='brown';
+list[i].style.padding='5px';
+list[i].style.borderRadius='5px';
+list[i+1].style.padding='5px';
+list[i+1].style.borderRadius='5px';
+list[i+1].style.margin='5px';
+list[i].style.margin='5px'
+
+
+}
