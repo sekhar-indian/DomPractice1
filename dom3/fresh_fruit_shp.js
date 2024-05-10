bc9b84e819f2dd96ddb6349c67c2c456d66d8def
sekhar <munisekharudavalapati1@gmail.com>
2024-05-10 18:56:10 +0530

    small submit form

--- dom3/fresh_fruit_shp.js
+++ dom3/fresh_fruit_shp.js
@@ -1,26 +1,38 @@
-const header=document.querySelector('#main-heading');
-header.style.textAlign='right';
+// const header=document.querySelector('#main-heading');
+// header.style.textAlign='right';
 
-const listheading=document.querySelector('#basket-heading');
-listheading.style.color='brown';
-listheading.style.padding='10px';
+// const listheading=document.querySelector('#basket-heading');
+// listheading.style.color='brown';
+// listheading.style.padding='10px';
 
-const lists=document.querySelector('.fruits');
-lists.style.backgroundColor='grey';
-lists.style.listStyleType='none';
-lists.style.borderRadius='10px';
-lists.style.padding='10px';
+// const lists=document.querySelector('.fruits');
+// lists.style.backgroundColor='grey';
+// lists.style.listStyleType='none';
+// lists.style.borderRadius='10px';
+// lists.style.padding='10px';
 
-const list=document.querySelectorAll('.fruit');
-for(let i=0;i<list.length-1;i=i+2){
-list[i].style.backgroundColor='white';
-list[i+1].style.backgroundColor='brown';
-list[i].style.padding='5px';
-list[i].style.borderRadius='5px';
-list[i+1].style.padding='5px';
-list[i+1].style.borderRadius='5px';
-list[i+1].style.margin='5px';
-list[i].style.margin='5px'
+// const list=document.querySelectorAll('.fruit');
+// for(let i=0;i<list.length-1;i=i+2){
+// list[i].style.backgroundColor='white';
+// list[i+1].style.backgroundColor='brown';
+// list[i+1].style.color='white';
+// list[i].style.padding='5px';
+// list[i].style.borderRadius='5px';
+// list[i+1].style.padding='5px';
+// list[i+1].style.borderRadius='5px';
+// list[i+1].style.margin='5px';
+// list[i].style.margin='5px'
 
 
-}
+// }
+
+
+let el=document.createElement('h3');
+let text=document.createTextNode('Buy high quality organic fruits online');
+el.appendChild(text);
+console.log(el);
+
+ 
+
+
+
