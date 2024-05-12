656ea33f99d44dc89ecf96ca1b52e6a1922561c5
sekhar <munisekharudavalapati1@gmail.com>
2024-05-12 13:18:05 +0530

    Having Delete Functionality

---
+++ dom15/a.js
@@ -0,0 +1,38 @@
+function handleFormSubmit(event){
+    event.preventDefault();
+    const username=event.target.username.value;
+    const email=event.target.email.value;
+    const phone=event.target.phone.value;
+
+    const element=document.querySelector('body');
+    const newElement=document.createElement('li');
+    newElement.id='sekhar'
+    const data1=document.createTextNode(`${username}- ${email}- ${phone}`);
+    newElement.appendChild(data1);
+
+    //deleting data elemnt add 
+    const delete1=document.createElement('input');
+    delete1.setAttribute('type','submit');
+    delete1.setAttribute('onclick','deteing(event)');
+    delete1.value='delete';
+    newElement.appendChild(delete1)
+
+    //deleting data 
+   
+
+    element.append(newElement)
+    // add data in local storage
+    localStorage.setItem(username,JSON.stringify({email:email,phone:phone}));
+
+
+  }
+
+  function deteing(event){
+   
+   //complete this 
+   const listItem = event.target.parentNode; 
+   const username = listItem.textContent.split('-')[0].trim(); 
+   listItem.remove();
+   localStorage.removeItem(username)
+  console.log(username)
+  }
